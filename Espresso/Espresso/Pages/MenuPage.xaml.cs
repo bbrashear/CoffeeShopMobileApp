4603b2d4e7eea241eb73ee9097915f118cc1c93d
Blake Brashear <bbrashear94@gmail.com>
2020-08-24 13:49:36 -0400

    Created menu page UI and bound data from Menu class to the UI

--- Espresso/Espresso/Pages/MenuPage.xaml.cs
+++ Espresso/Espresso/Pages/MenuPage.xaml.cs
@@ -1,20 +1,43 @@
-﻿using System;
+﻿using Espresso.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Menu = Espresso.Models.Menu;
 
 namespace Espresso.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MenuPage : ContentPage
     {
+        public ObservableCollection<Menu> Menus;
+        public static bool First = true;
         public MenuPage()
         {
             InitializeComponent();
+            Menus = new ObservableCollection<Menu>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (First)
+            {
+                ApiServices apiServices = new ApiServices();
+                var menus = await apiServices.GetMenu();
+                foreach (var menu in menus)
+                {
+                    Menus.Add(menu);
+                }
+
+                LvMenu.ItemsSource = Menus;
+            }
+            First = false;
         }
     }
 }
