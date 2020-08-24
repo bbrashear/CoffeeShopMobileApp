394288e675a518b7bba953daa5808b269ec6fef7
Blake Brashear <bbrashear94@gmail.com>
2020-08-24 14:35:58 -0400

    Created Reservation page UI and added code to submit a reservation

--- Espresso/Espresso/Pages/ReservationPage.xaml.cs
+++ Espresso/Espresso/Pages/ReservationPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Espresso.Models;
+using Espresso.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,30 @@ namespace Espresso.Pages
         public ReservationPage()
         {
             InitializeComponent();
+        }
+
+        private async void BtnBookTable_Clicked(object sender, EventArgs e)
+        {
+            Reservation reservation = new Reservation()
+            {
+                Name = EntName.Text,
+                Email = EntEmail.Text,
+                Phone = EntPhone.Text,
+                TotalPeople = EntTotalPeople.Text,
+                Date = DpBookingDate.Date.ToString(),
+                Time = TpBookingTime.Time.ToString(),
+            };
+            ApiServices apiServices = new ApiServices();
+            bool response = await apiServices.ReserveTable(reservation);
+            if (response != true)
+            {
+                await DisplayAlert("Oops", "Something went wrong", "Okay");
+            }
+            else
+            {
+                await DisplayAlert("Hi", "Your table has been reserved", "Okay");
+            }
+
         }
     }
 }
