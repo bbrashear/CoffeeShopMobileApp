94355fbccd27ab7f147b0ccbf07d3fe5ab64f2f4
Blake Brashear <bbrashear94@gmail.com>
2020-08-24 12:19:19 -0400

    added ReserveTable method to ApiServices class

--- Espresso/Espresso/Services/ApiServices.cs
+++ Espresso/Espresso/Services/ApiServices.cs
@@ -13,8 +13,17 @@ namespace Espresso.Services
         public async Task<List<Menu>> GetMenu()
         {
             var client = new HttpClient();
-            var response = await client.GetStringAsync("https://blakeespressoapi.azurewebsites.net/api/Menus");
-            return JsonConvert.DeserializeObject<List<Menu>>(response);
+            var response = await client.GetStringAsync("https://blakeespressoapi.azurewebsites.net/api/Menus"); //htttp get request, async allows the ui to continue functioning
+            return JsonConvert.DeserializeObject<List<Menu>>(response); //converts json data into c# object
+        }
+
+        public async Task<bool> ReserveTable(Reservation reservation)
+        {
+            var client = new HttpClient();
+            var json = JsonConvert.SerializeObject(reservation); //turns c# object into json format
+            var content = new StringContent(json, Encoding.UTF8, "application/json"); //creates a formatted text appropriate for the http server or client communication
+            var response = await client.PostAsync("https://blakeespressoapi.azurewebsites.net/api/Reservations", content); //http post request
+            return response.IsSuccessStatusCode;
         }
     }
 }
